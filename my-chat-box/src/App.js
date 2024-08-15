556222a9eeb317b39b54ac16883a1683338a38e3
byunn90 <kayhanturk90@Live.com>
2024-08-15 11:59:00 +1000

    backend

--- my-chat-box/src/App.js
+++ my-chat-box/src/App.js
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ChatBox from "./components/ChatBox";
 import OpenChatBox from "./components/OpenChatBox";
 
 function App() {
   const [chat, setChat] = useState([]);
   const [isChatOpen, setIsChatOpen] = useState(false);
+
+  // Fetch messages when the chatbox is opened
+  useEffect(() => {
+    if (isChatOpen) {
+      console.log("Open");
+      fetch("http://localhost:5000/api/message")
+        .then((response) => response.json())
+        .then((data) => setChat(data))
+        .catch((error) => console.error("hi:", error));
+    } else {
+      console.log("Closed");
+    }
+  }, [isChatOpen]); // Dependency array ensures this runs when isChatOpen changes
 
   const handleChatToggle = () => {
     setIsChatOpen(!isChatOpen);
