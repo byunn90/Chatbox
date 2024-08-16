eea3fcab420b0d18ba9c3fd27ce7cdd190f77717
byunn90 <kayhanturk90@Live.com>
2024-08-16 15:27:05 +1000

    ,

--- my-chat-box/src/App.js
+++ my-chat-box/src/App.js
@@ -3,14 +3,20 @@ import ChatBox from "./components/ChatBox";
 import OpenChatBox from "./components/OpenChatBox";
 
 function App() {
+  const [name, SetName] = "";
   const [chat, setChat] = useState([]);
   const [isChatOpen, setIsChatOpen] = useState(false);
   ////// Needs work done
   // Fetch messages when the chatbox is opened
+
+  // if (!name) {
+  //   alert("Please enter your Name before sending a message");
+  // }
+
   useEffect(() => {
     if (isChatOpen) {
       console.log("Open");
-      fetch("http://localhost:5000/api/message")
+      fetch("http://localhost:5228/chatbox")
         .then((response) => response.json())
         .then((data) => setChat(data))
         .catch((error) => console.error("hi:", error));
