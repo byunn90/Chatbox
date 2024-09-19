512657bad6a9b45a6e546d3a4c8d0a8d3f17f580
byunn90 <kayhanturk90@Live.com>
2024-09-19 14:28:34 +1000

    ,

---
+++ my-chat-box/src/components/CloseChatBox.js
@@ -0,0 +1,7 @@
+function closeChatBox({ currentQuestion }) {
+  if (currentQuestion === "No") {
+  } else if (currentQuestion === "Yes") {
+  } else if (currentQuestion === "No") {
+  }
+}
+export default closeChatBox;
