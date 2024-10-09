49a70846a81899b8aabaec6838ccfc195825f67e
byunn90 <kayhanturk90@Live.com>
2024-10-09 11:19:00 +1100

    questions

--- ChatBoxServer/obj/Debug/net8.0/ChatBoxServer.AssemblyInfo.cs
+++ ChatBoxServer/obj/Debug/net8.0/ChatBoxServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBoxServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4b80ecf741df03ffc65764161136dcc240efd61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82929f94ec997972499afa84ce7662411a8ccc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBoxServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBoxServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
