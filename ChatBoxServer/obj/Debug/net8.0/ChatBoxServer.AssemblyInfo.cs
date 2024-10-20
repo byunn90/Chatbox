48f8cf3e05e412564fee643515164183502d122b
byunn90 <kayhanturk90@Live.com>
2024-10-20 17:46:33 +1100

    ,

--- ChatBoxServer/obj/Debug/net8.0/ChatBoxServer.AssemblyInfo.cs
+++ ChatBoxServer/obj/Debug/net8.0/ChatBoxServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBoxServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e96bb92c745dfdd74820b78d2c6de4e3d75a0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574e1301bf9dddf9c8731cf85e9f22aee974fc0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBoxServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBoxServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
